5b114c36be0609fca26676db1bcfb162f9877d99
jchanpark <junpark4936@gamil.com>
2022-03-22 18:43:32 -0400

    Redirecting short RULs to its longURLs completed

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 const generateRandomString = function() {
-  Math.random().toString(36).substring(2,8);
+  return Math.random().toString(36).substring(2,8);
 };
 
 const urlDatabase = {
@@ -34,13 +34,22 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  // console.log('req.body:', req.body);
+  const shortURL = generateRandomString();
+  console.log('shorURL', shortURL);
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+  // console.log('urlDatabase[req.params.shortURL]', urlDatabase[req.params.shortURL]);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 })
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  res.redirect(urlDatabase[shortURL]);
+});
 
 
